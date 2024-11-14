d06062ef6023f6ccea48d3c486bc7245d4ec1784
RassielMex <rhcp94.rm@gmail.com>
2024-11-14 10:34:46 -0600

    Feat: welcome animation

--- src/app/components/home/welcome.tsx
+++ src/app/components/home/welcome.tsx
@@ -1,16 +1,36 @@
+"use client";
 import Image from "next/image";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export default function Welcome() {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end start"],
+  });
+  const springX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
+  const posX = useTransform(springX, [0, 0.5, 1], [-100, 0, 100]);
+
   return (
     <section className="py-6 h-screen">
       <p className="font-normal text-xs text-center mb-4">BIENVENIDO A</p>
       <h1 className="font-bold text-5xl text-center mb-4">FANTASTIC STORE</h1>
-      <div className="h-3/4 flex justify-center gap-4 overflow-hidden">
+      <motion.div
+        style={{ x: posX }}
+        className="h-3/4 flex justify-center gap-4 overflow-visible"
+        ref={containerRef}
+      >
         <CardImage src="/images/welcome1.jpg" />
         <CardImage src="/images/welcome2.jpg" />
         <CardImage src="/images/welcome3.jpg" />
         <CardImage src="/images/welcome4.jpg" />
-      </div>
+      </motion.div>
     </section>
   );
 }
