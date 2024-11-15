cc93d12da106c87e09effff44bb9482a8528c09b
RassielMex <rhcp94.rm@gmail.com>
2024-11-15 15:14:40 -0600

    Fat: opacity

--- src/app/ui/home/video-hero.tsx
+++ src/app/ui/home/video-hero.tsx
@@ -1,7 +1,7 @@
 export default function VideoHero() {
   return (
-    <section className="sticky top-0 -z-10 ">
-      <video width="full" height="full" autoPlay muted loop preload="auto">
+    <section className="sticky top-0 -z-10 opacity-65">
+      <video autoPlay muted loop preload="auto">
         <source src="/videos/home.mp4" type="video/mp4" />
         <track
           src="/path/to/captions.vtt"
