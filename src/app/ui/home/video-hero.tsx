121704594a23bbb7406d902de5f8269aa2ffd5c9
RassielMex <rhcp94.rm@gmail.com>
2024-11-15 15:37:04 -0600

    Feat: video object cover

--- src/app/ui/home/video-hero.tsx
+++ src/app/ui/home/video-hero.tsx
@@ -1,7 +1,13 @@
 export default function VideoHero() {
   return (
-    <section className="sticky top-0 -z-10 opacity-65">
-      <video autoPlay muted loop preload="auto">
+    <section className="sticky top-0 -z-10 opacity-65 h-screen">
+      <video
+        autoPlay
+        muted
+        loop
+        preload="auto"
+        className="object-cover h-full w-full"
+      >
         <source src="/videos/home.mp4" type="video/mp4" />
         <track
           src="/path/to/captions.vtt"
